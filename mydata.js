27a06ab0ded58ec6dd16a4d9883e8cf3f1b243a8
K Rajesh Kumar <kommirajesh@gmail.com>
2015-06-23 13:22:45 +0530

    Adding project to Github

---
+++ mydata.js
@@ -0,0 +1,52 @@
+mydata([
+{
+	"image":"images/gym1.jpg",
+	"name":"rajesh fitness center",
+	"location":"Gachibowli,Hyderabad"
+},
+{
+	"image":"images/gym2.jpg",
+	"name":"akshay fitness center",
+	"location":"Himayath nagar,Hyderabad"
+},
+{
+	"image":"images/gym3.jpg",
+	"name":"suvi fitness center",
+	"location":"madhapur,Hyderabad"
+},
+{
+	"image":"images/ozone.jpg",
+	"name":"ozone fitness center",
+	"location":"Banzara hills,Gachibowli"
+},
+{
+	"image":"images/truefitness.jpg",
+	"name":"truefitness enter",
+	"location":"Gandhinagar,Hyderabad"
+},
+{
+	"image":"images/Talwalkar.jpg",
+	"name":"Talwalkar Gym",
+	"location":"KauriHills,Hyderabad"
+},
+{
+	"image":"images/fitnessplus.png",
+	"name":"fitnessplus center",
+	"location":"Madinaguda,Hyderabad"
+},
+{
+	"image":"images/360.jpg",
+	"name":"360 DEGREE FITNESS GYM",
+	"location":"jublihills,Hyderabad"
+},
+{
+	"image":"images/apollo.gif",
+	"name":"APOLLO HEALTH",
+	"location":"film nagar,Hyderabad"
+},
+{
+	"image":"images/burn.jpg",
+	"name":"burn fitness gym",
+	"location":"secendrabad,Hyderabad"
+}
+])
